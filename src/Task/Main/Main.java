bd674117a5c7d6253b5b889b75df4570a38c6263
Denzap <65074578+Denzap0@users.noreply.github.com>
2020-08-06 10:02:00 +0300

    last

--- src/Task/Main/Main.java
+++ src/Task/Main/Main.java
@@ -7,30 +7,6 @@ import Task.Stores.CarsStore;
 public class Main {
 
     public static void main(String[] args) {
-        /*Car car1 = new DriftCar();
-        car1.move();
-        DriftCar car2 = new DriftCar();
-        car2.move();
-        car2.refueling(50);
-        car2.drift(5, 55);
-        Car car3 = new GasolineTanker();
-        car3.move();
-        GasolineTanker car4 = new GasolineTanker();
-        car4.move();
-        car4.fuelFill(400);
-        car4.fuelDrain(400);
-        System.out.println(car4.getFuelCapacity());
-        Movable anim1 = new Guepard();
-        anim1.move();
-        Guepard anim2 = new Guepard();
-        anim2.move();
-        anim2.sleep();
-        Movable anim3 = new Eagle();
-        anim3.move();
-        Eagle anim4 = new Eagle();
-        anim4.setFlightSpeed(240);
-        anim4.setFlightAltitude(100);
-        anim4.move();*/
 
         DriftCar[] cars = new DriftCar[10];
         for(int i = 0; i < 10; i++){
